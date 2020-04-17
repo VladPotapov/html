2996dce793c747684f4e43b7b5dd8cce1e6f90d9
Roman <kotelnikovrm@gmail.com>
2020-04-17 17:28:48 +0800

    осваивая xmlhttprequest понял как работает метод get

--- 11/script.js
+++ 11/script.js
@@ -1,3 +1,44 @@
-var xml = new XMLHttpRequest();
+function method_get() {
+    let xhr = new XMLHttpRequest();
+    
+    //промежуток времени, который мы готовы ждать ответ
+    //10 секунд
+    xhr.timeout = 10000;
 
-console.log(xml);
+    //инициализация
+    //true указывает что запрос асинхронный
+    xhr.open("GET", "get.php");
+
+    //запрос
+    xhr.send();
+
+    xhr.onload = function() {
+        if(xhr.status != 200) {
+            console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+        }
+        else {
+            //показывает статус и содержимое страницы
+            console.log(`Загружено: ${xhr.status} \n ${xhr.response}`);
+            console.log(`Готово получили ${xhr.response.length} байт`);
+        }
+    };
+
+    xhr.onprogress = function(event) {
+        if(event.lengthComputable) {
+            console.log(`Загружено: ${event.loaded} из ${event.total}`);
+            console.log(`lengthComputable = ${event.lengthComputable}`);
+        }
+        else {
+            console.log(`Получено ${event.loaded} байт`);
+        }
+    };
+
+    xhr.onerror = function() {
+        alert(`ошибка соединения`);
+    };
+
+
+    console.log(xhr);
+}
+
+method_get();
--- 11/xhr.js
+++ 11/xhr.js
@@ -1,14 +1,15 @@
 let xhttp = new XMLHttpRequest();
-
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        myFun(this.resposeText);
+xhttp.onreadystatechange = function () {
+    if ( this.readyState == 4 && this.status == 200 ) {
+        myFunction(this.responseText);
     }
-}
+};
 
-xhttp.open("GET", "file.php", true);
+xhttp.open("GET", "get.php", true);
 xhttp.send();
 
-function myFun(data) {
+function myFunction (data) {
     console.log(data);
 }
+
+console.log('step1');
